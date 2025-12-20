dbee0e5492be2ff122c8f7f92430c00f6f1073c2
Khushi Sahu <khushii77e@gmail.com>
2025-12-20 23:48:16 +0530

    Update max frequency optimal solution (Array I)

--- Arrays/Array-I/max_frequency(optimal).java
+++ Arrays/Array-I/max_frequency(optimal).java
@@ -8,15 +8,22 @@ class MaxFrequency {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
-        int maxCount = 0;
-        int element = arr[0];
+       int maxCount = 0;
 
-        // we find element with max-frequency
+        // we find max-frequency
         for (int key : map.keySet()) {
             if (map.get(key) > maxCount) {
                 maxCount = map.get(key);
-                element = key;
             }
         }
+        
+        int ans = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) == maxCount) {
+                ans += maxCount;
+            }
+        }
+
+        return ans; 
     }
 }
