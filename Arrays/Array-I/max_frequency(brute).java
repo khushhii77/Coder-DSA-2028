503981f3734629c70a4e415efcf63a35a3ba2ca3
Khushi Sahu <khushii77e@gmail.com>
2025-12-20 23:46:48 +0530

    Update max frequency brute solution (Array I)

--- Arrays/Array-I/max_frequency(brute).java
+++ Arrays/Array-I/max_frequency(brute).java
@@ -1,24 +1,38 @@
 // Topic: Array I
 // Problem: Count element with maximum frequency
 // Approach: Brute force frequency counting
-// Time Complexity: O(n^2)
+// Time Complexity: O(n^2) .... //(n^2+n^2)
 // Space Complexity: O(1)
 
 class MaxFrequency {
     static void maxFrequency(int[] arr) {
         int maxCount = 0;
-        int element = arr[0];
-
+        
+     //we find max freq..
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[i] == arr[j])
                     count++;
             }
-            if (count > maxCount) {
-                maxCount = count;
-                element = arr[i];
+            maxCount = Math.max(maxCount, count);
+        }
+        
+           // sum frequencies of elements having maxCount
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count == maxCount) {
+                total += count;
             }
         }
+
+        return total;
     }
 }
