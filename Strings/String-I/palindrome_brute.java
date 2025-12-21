df62332b943021f2a60a38d603a0bf3895016806
Khushi Sahu <khushii77e@gmail.com>
2025-12-21 22:04:06 +0530

    Add palindrome check (brute) - String I

--- Strings/String-I/palindrome_brute.java
+++ Strings/String-I/palindrome_brute.java
@@ -1,14 +1,20 @@
 // Topic: String I
 // Problem: Palindrome Check (Brute)
 // Time: O(n)
-// Space: O(n) [as we use rev to store]
+// Space: O(n) [as we use another empty string to store]
 
 class PalindromeBrute {
     static boolean isPalindrome(String s) {
+      String t = "";
+        for (char c : s.toCharArray())
+            if (Character.isLetterOrDigit(c))
+                t += Character.toLowerCase(c);
+
         String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev += s.charAt(i);
+
+        for (int i = t.length() - 1; i >= 0; i--) {
+         rev += t.charAt(i);
         }
-        return s.equals(rev);
+           return t.equals(rev);
     }
 }
