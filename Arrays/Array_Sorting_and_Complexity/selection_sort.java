96aae2116b8346fbec2d8de9a6ab11da04fc30aa
Khushi Sahu <khushii77e@gmail.com>
2025-12-21 12:40:07 +0530

    Add selection sort implementation

---
+++ Arrays/Array_Sorting_and_Complexity/selection_sort.java
@@ -0,0 +1,22 @@
+// Selection Sort
+// Time: O(n^2), Space: O(1)
+
+class SelectionSort {
+    static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
