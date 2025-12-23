4fdb72d2c775d42975e2c55fa0f7b35b017a03f5
Khushi Sahu <khushii77e@gmail.com>
2025-12-24 00:15:32 +0530

    Add reverse linked list

--- LinkedList/LinkedList-I/ReverseLinkedList.java
+++ LinkedList/LinkedList-I/ReverseLinkedList.java
@@ -1,3 +1,10 @@
+// Topic: Linked List I
+// Problem: Reverse a Linked List
+// Description: Given the head of a singly linked list, reverse the list.
+// Approach: Iterative Pointer Reversal (Optimal)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
 class ReverseLinkedList {
     static class Node {
         int data;
@@ -7,17 +14,18 @@ class ReverseLinkedList {
             next = null;
         }
     }
-  
-    static Node reverseList(Node head) {
+
+    public static Node reverse(Node head) {
         Node prev = null;
         Node curr = head;
 
         while (curr != null) {
-            Node nextNode = curr.next; 
-            curr.next = prev;         
-            prev = curr;              
-            curr = nextNode;           
+            Node nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
         }
-        return prev;
+
+        return prev; 
     }
 }
