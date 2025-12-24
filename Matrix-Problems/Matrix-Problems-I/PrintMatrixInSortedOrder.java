5e5a040e2235618bd6829fc957dd856301cb4778
Khushi Sahu <khushii77e@gmail.com>
2025-12-24 23:18:02 +0530

    Add solution to print matrix elements in sorted order

---
+++ Matrix-Problems/Matrix-Problems-I/PrintMatrixInSortedOrder.java
@@ -0,0 +1,44 @@
+// Topic: Matrix I
+// Problem: Print elements in sorted order
+// Description:Sort an array without using built-in sort
+// Approach: Merge Sort
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
+class SortArray {
+
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    void merge(int[] arr, int l, int mid, int r) {
+        int[] temp = new int[r - l + 1];
+        int i = l, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= r) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= r)
+            temp[k++] = arr[j++];
+
+        for (int x = 0; x < temp.length; x++)
+            arr[l + x] = temp[x];
+    }
+}
