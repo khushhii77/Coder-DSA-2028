75b4a296df4c63b92cd9ae2e85b369363f168f3d
Khushi Sahu <khushii77e@gmail.com>
2025-12-26 15:22:13 +0530

    ADD NextGreaterElement Problem

---
+++ Stack/Stack Implementation/NextGreaterElement.java
@@ -0,0 +1,30 @@
+// Topic: Stack Implementation
+// Problem: Next Greater Element I
+// Description: For each element in nums1 find next greater in nums2
+// Approach: Monotonic stack + hashmap
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.*;
+
+class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> mp = new HashMap<>();
+
+        for(int x : nums2){
+            while(!st.isEmpty() && st.peek() < x){
+                mp.put(st.pop(), x);
+            }
+            st.push(x);
+        }
+
+        int ans[] = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = mp.getOrDefault(nums1[i], -1);
+        }
+
+        return ans;
+    }
+}
