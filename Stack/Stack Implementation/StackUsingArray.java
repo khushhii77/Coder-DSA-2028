7f5cf1f5e8a5d3dee012ca715c362fec3b5b04d9
Khushi Sahu <khushii77e@gmail.com>
2025-12-26 15:20:06 +0530

    add Stack Using Array Problem

---
+++ Stack/Stack Implementation/StackUsingArray.java
@@ -0,0 +1,29 @@
+// Topic: Stack Implementation
+// Problem: Implement Stack using Array
+// Description: Implement stack supporting push, pop, top and isEmpty operations
+// Approach: Maintain top pointer
+// Time Complexity: O(1)
+// Space Complexity: O(n)
+
+class StackUsingArray {
+    int top;
+    int arr[];
+
+    StackUsingArray(int n){
+        arr = new int[n];
+        top = -1;
+    }
+    boolean isEmpty(){
+        return top == -1;
+    }
+    void push(int x){
+        arr[++top] = x;
+    }
+    int pop(){
+        return arr[top--];
+    }
+
+    int peek(){
+        return arr[top];
+    }
+}
